3d70180fccc8f40d79ee1a78947b0a6518f807ad
naomiisabel <86689323+naomiisabel@users.noreply.github.com>
2021-06-29 17:27:25 -0500

    Add files via upload

---
+++ CONEXION.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaTutores.DATOS
+{
+    class CONEXION
+    {
+        SqlConnection con;
+        public CONEXION()
+        {
+            con = new SqlConnection("Server=DESKTOP-P00P1CC;Database=Tutores_db;integrated security=true");
+        
+        }
+        public SqlConnection conectar()
+        {
+            try
+            {
+                con.Open();
+                return con;
+            }
+            catch(Exception e)
+            {
+                return null;
+            }
+            
+        }
+        public bool desconectar()
+        {
+            try
+            {
+                con.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
